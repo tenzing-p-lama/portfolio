c142d6dbf864e25f6d22a40c443c69814c15e358
Tenzing P Lama <tenzingpalden520@gmail.com>
2024-01-05 12:59:15 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <section className="main-contact">
+          <h4>Fullstack Developer</h4>
+        </section>
+
+        <section className="main-header">
+          <h1>Tenzing</h1>
+          <h1>Palden</h1>
+          <h1>Lama</h1>
+        </section>
+      </header>
+    </div>
+  );
+}
+
+export default App;
