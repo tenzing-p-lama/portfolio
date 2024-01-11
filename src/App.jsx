5e596293b11cad593ea8f91c566cec605072ef5e
Tenzing P Lama <tenzingpalden520@gmail.com>
2024-01-11 12:24:57 -0500

    style main section and add AboutME

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
+import AboutMe from "./components/AboutMe/AboutMe";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <section className="main-contact">
-          <h4>Fullstack Developer</h4>
-        </section>
+    <div>
+      <header className="main-container">
+        <div className="main">
+          <section className="main-contact">
+            <h4>Fullstack Developer</h4>
+            <div className="main-contact__links">
+              <p>LinkedIn</p>
+              <p>GitHub</p>
+              <p>Email</p>
+            </div>
+          </section>
 
-        <section className="main-header">
-          <h1>Tenzing</h1>
-          <h1>Palden</h1>
-          <h1>Lama</h1>
-        </section>
+          <section className="main-header">
+            <h1>Tenzing</h1>
+            <h1>Palden</h1>
+            <h1>Lama</h1>
+          </section>
+        </div>
       </header>
+
+      <AboutMe />
     </div>
   );
 }
---
+++ src/components/AboutMe/AboutMe.jsx
@@ -0,0 +1,16 @@
+import "./AboutMe.scss";
+
+function AboutMe() {
+  return (
+    <div className="about">
+      <p className="about__copy">
+        Hi, I'm a recent Web Development Diploma graduate. With a background in
+        graphic design and a passion for art and photography, I have a keen eye
+        for aesthetics and user experience, as well as the technical skills to
+        build responsive and interactive websites and applications.
+      </p>
+    </div>
+  );
+}
+
+export default AboutMe;
