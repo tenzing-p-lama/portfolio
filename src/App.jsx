78e586aff788bc52d25f6404c94795575e0cfe8e
Tenzing P Lama <tenzingpalden520@gmail.com>
2024-01-12 12:20:23 -0500

    add NavBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import "./App.scss";
 import AboutMe from "./components/AboutMe/AboutMe";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div>
+      <NavBar />
+
       <header className="main-container">
         <div className="main">
           <section className="main-contact">
-            <h4>Fullstack Developer</h4>
+            <h2>Fullstack Developer</h2>
             <div className="main-contact__links">
               <p>LinkedIn</p>
               <p>GitHub</p>
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import "./NavBar.scss";
+
+function NavBar() {
+  return (
+    <div className="nav">
+      <p className="nav-menu">About Me</p>
+      <p className="nav-menu">Projects</p>
+      <p className="nav-menu">My Stack</p>
+      <p className="nav-menu">Let's Talk</p>
+    </div>
+  );
+}
+
+export default NavBar;
