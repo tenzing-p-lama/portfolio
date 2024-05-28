0b99c87cc4bf6b5be8c5d3c71d6d47d19ef2c923
Tenzing P Lama <tenzingpalden520@gmail.com>
2024-05-28 12:01:32 -0700

    nav style

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,40 +2,50 @@ import React, { useState, useEffect } from "react";
 import "./NavBar.scss";
 import Hamburger from "../Hamburger/Hamburger";
 
-function NavBar() {
+const NavBar = () => {
   const [hamburger, setHamburger] = useState(false);
   const toggleHamburger = () => {
     setHamburger(!hamburger);
+    document.body.classList.toggle("no-scroll", !hamburger);
   };
-
+  const closeHamburger = () => {
+    setHamburger(false);
+    document.body.classList.remove("no-scroll");
+  };
   useEffect(() => {
     setHamburger(false);
+    document.body.classList.remove("no-scroll");
   }, []);
 
   return (
-    <div className="nav">
+    <div className="nav__mobile">
       <div className="hamburgerMenu" onClick={toggleHamburger}>
         <Hamburger isOpen={hamburger} />
       </div>
 
-      <a href="/" className="nav-menu">
-        Home
-      </a>
-      <a href="#about" className="nav-menu">
-        About Me
-      </a>
-      <a href="#projects" className="nav-menu">
-        Projects
-      </a>
-      <a href="#mystack" className="nav-menu">
-        My Stack
-      </a>
+      <ul className={hamburger ? "nav-links" : "nav-links__close"}>
+        <a href="/" className="nav-menu" onClick={closeHamburger}>
+          Home
+        </a>
 
-      <a href="#contact" className="nav-menu">
-        Let's Talk
-      </a>
+        <a href="#about" className="nav-menu" onClick={closeHamburger}>
+          About Me
+        </a>
+
+        <a href="#projects" className="nav-menu" onClick={closeHamburger}>
+          Projects
+        </a>
+
+        <a href="#mystack" className="nav-menu" onClick={closeHamburger}>
+          My Stack
+        </a>
+
+        <a href="#contact" className="nav-menu" onClick={closeHamburger}>
+          Let's Talk
+        </a>
+      </ul>
     </div>
   );
-}
+};
 
 export default NavBar;
